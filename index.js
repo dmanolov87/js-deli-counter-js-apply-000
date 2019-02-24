dd09ff387c28dd0399b5a277219226ef95d8cc9c
Daniela Manolova <daniela.manolova@blackrock.com>
2019-02-24 20:09:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function takeANumber(katzDeliLine, name) {
-  var katzDeliLine = [];
   katzDeliLine.push(name);
   return "Welcome, " + name + ". You are number " + (katzDeliLine.indexOf(name) +1) + " in line."
 }
